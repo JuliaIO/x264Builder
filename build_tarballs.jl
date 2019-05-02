07e69d0114eb98fa9fabe6ce5608fda3268e4538
Ian Butterworth <i.r.butterworth@gmail.com>
2019-05-02 15:47:59 -0400

    reformat platform list (to match other builders)

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -25,19 +25,24 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
+    # Windows
+    Windows(:i686),
+    Windows(:x86_64),
+
+    # linux
     Linux(:i686, :glibc),
     Linux(:x86_64, :glibc),
     Linux(:aarch64, :glibc),
-    Linux(:armv7l, :glibc, :eabihf),
+    Linux(:armv7l, :glibc),
     Linux(:powerpc64le, :glibc),
+
+    # musl
     Linux(:i686, :musl),
     Linux(:x86_64, :musl),
-    Linux(:aarch64, :musl),
-    Linux(:armv7l, :musl, :eabihf),
-    MacOS(:x86_64),
+
+    # The BSD's
     FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
+    MacOS(:x86_64),
 ]
 
 # The products that we will ensure are always built
